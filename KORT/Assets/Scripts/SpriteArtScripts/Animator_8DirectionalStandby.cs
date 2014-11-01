3f61bb436e9be2408a565c14c0d6c883395f7b2e
HiroXHeartless <lea2@rpi.edu>
2014-11-01 17:46:20 -0400

    Testing Sprites

---
+++ KORT/Assets/Scripts/SpriteArtScripts/Animator_8DirectionalStandby.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Animator_8DirectionalStandby : MonoBehaviour {
+	public Sprite[] sprites;
+	public int current_sprite;
+
+	private SpriteRenderer sr;
+
+	void Awake() {
+		sr = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (current_sprite >= 7)
+		{
+			current_sprite = 0;
+		} 
+
+		sr.sprite = sprites[current_sprite];
+
+	}
+}
---
+++ KORT/Assets/Scripts/SpriteArtScripts/MyAnimator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyAnimator : MonoBehaviour {
+	public float frames_per_second = 2.0f;
+	public Sprite[] sprites;
+	public int current_sprite;
+	
+	private float counter = 0.0f;
+	private SpriteRenderer sr;
+	
+	void Awake() {
+		sr = GetComponent<SpriteRenderer>();
+	}
+	void Update() {
+		counter += Time.deltaTime;
+		current_sprite += Mathf.FloorToInt(counter * frames_per_second);
+		current_sprite %= sprites.Length;
+		counter = (counter * frames_per_second) % 1.0f / frames_per_second;
+		sr.sprite = sprites[current_sprite];
+	}
+}
