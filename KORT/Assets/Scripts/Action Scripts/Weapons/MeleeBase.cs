1b6b68a33252356b8912b957372b3e5349bc08c3
Robert Wild <wildr2@rpi.edu>
2014-11-03 20:40:53 -0500

    Add melee weapon raycast collision

--- KORT/Assets/Scripts/Action Scripts/Weapons/MeleeBase.cs
+++ KORT/Assets/Scripts/Action Scripts/Weapons/MeleeBase.cs
@@ -4,15 +4,69 @@ using System.Collections.Generic;
 
 public class MeleeBase : WeaponBase
 {
-    // Combat animation variables
-    public float animation_length = 0.2f; // the time in seconds it takes for the
-                                  //   the attack animation to complete.
-    public float animation_start;        // the time in seconds when the animation
-                                  //   began.
+    // Animation
+
+    // the time in seconds it takes for the attack animation to complete
+    public float animation_length = 0.2f; 
+    // the time in seconds when the animation begin
+    public float animation_start;        
     protected bool in_animation = false;
-    public SpriteRenderer animation; // the renderer with the actual animation
+    public SpriteRenderer animation;
+
+
+    // Collision
+
+    // (Later there could be derived classes for swing / thrust weapons...) 
+    protected float swing_start_radius = 3f, swing_radius = 9.6f; 
+    protected float swing_angle_start = Mathf.PI / 4f, swing_angle_end = Mathf.PI * 3 / 4f;
+
+    // angle between rays
+    private float ray_precision = Mathf.PI / 16f;
+    protected float[] ray_cast_angles; // references angles (character aiming to the right)
     
-    public GameObject character_group; // reference to the characters object in the hierarchy that we will be get the characters from.
+    // layer specifying which objects can be hit by this weapon (maybe have it on the infohub...)
+    public LayerMask collision_layer; 
+    //public GameObject character_group; // reference to the characters object in the hierarchy that we will be get the characters from.
+
+
+
+    public void Start()
+    {
+        weapon_name = "Weapon Melee";
+        aim_info_hub = transform.parent.GetComponentInChildren<CharAimInfoHub>();
+
+
+        // collision physics preperations
+        PrepareRaycastDirections();
+        
+    }
+    private void PrepareRaycastDirections()
+    {
+        float total_swing_angle = swing_angle_end - swing_angle_start;
+        int n = (int)Mathf.Ceil(total_swing_angle / ray_precision);
+
+        ray_cast_angles = new float[n];
+        float inter_angle = total_swing_angle / (n-1);
+
+        for (int i = 0; i < n; ++i)
+        {
+            // reference angles for if the character was aim to the right
+            ray_cast_angles[i] = (swing_angle_start + inter_angle * i) - (Mathf.PI / 2f);
+        }
+    }
+
+    public void Update()
+    {
+        DebugDrawRayCasts();
+
+        //Debug.Log("animation");
+        if (in_animation && (Time.time - animation_start) > animation_length)
+        {
+            in_animation = false;
+            //animation.enabled = false;
+            animation.color = Color.white * 0.3f;
+        }
+    }
 
     // Override RunAttack() from WeaponBase 
     public override void RunAttack()
@@ -25,8 +79,9 @@ public class MeleeBase : WeaponBase
             // If they have, do all the stuff that needs to happen when attack
             //   is run.
             last_attack = Time.time;
-            HandleCollision();
             HandleAnimation();
+            HandleCollision();
+            
             // Debug.Log("Melee Attack with " + weapon_name);
         }
     }
@@ -39,7 +94,38 @@ public class MeleeBase : WeaponBase
         /// they've been hit by the attack and should do some sort of 
         /// damage thing.
         //Debug.Log("Check for Collisions");
+
+        HashSet<RaycastHit2D> all_colliders = new HashSet<RaycastHit2D>();
+
+        for (int i = 0; i < ray_cast_angles.Length; ++i)
+        {
+            float a = ray_cast_angles[i] + aim_info_hub.GetAimRotation();
+            Vector2 ray = new Vector2(Mathf.Cos(a), Mathf.Sin(a)).normalized;
+
+            RaycastHit2D[] cols = Physics2D.RaycastAll((Vector2)transform.position + ray * swing_start_radius,
+                ray, swing_radius, collision_layer);
+
+            all_colliders.UnionWith(cols);
+        }
+
+
         
+
+        foreach (RaycastHit2D hit in all_colliders)
+        {
+            Debug.Log("HIT " + hit.collider.name);
+            animation.color = Color.red;
+
+            Character c = hit.collider.GetComponent<Character>();
+            if (c)
+            {
+                c.Hit(Vector2.zero, true);
+            }
+        }
+        
+
+
+        /*
         //Debug.Log(character_array);
         foreach (Transform child in character_group.transform)
         {
@@ -49,26 +135,10 @@ public class MeleeBase : WeaponBase
 
             if (1f < distance && distance < 9f)
             {
-                //Debug.Log("Distance: "+distance);
-                //Debug.Log("  other: " + other_position);
-                /*
-                float angle = GeneralHelpers.AngleBetweenVectors((Vector2)transform.position, other_position);
-                angle = Mathf.Rad2Deg*angle;
-                float direction = aim_info_hub.GetAimRotation() * Mathf.Rad2Deg;
-                Debug.Log(angle + "-" + direction);
-                angle = Mathf.Abs(angle - direction);
-                 * */
-                float angle = GeneralHelpers.AngleBetweenVectors(aim_info_hub.GetAimDirection(),
-                    child.position - transform.position);
-
-
-                if (angle < Mathf.PI/2f)
-                {
-                    Character c = child.GetComponent<Character>();
-                    if (c) c.Hit(Vector2.zero, true);
-                }
+                
             }
         }
+         * */
 
     }
 
@@ -82,23 +152,17 @@ public class MeleeBase : WeaponBase
         animation.enabled = true;
         in_animation = true;
         animation_start = Time.time;
+        animation.color = Color.white;
     }
 
-	// Use this for initialization
-	public void Start () 
+	private void DebugDrawRayCasts()
     {
-        weapon_name = "Weapon Melee";
-        aim_info_hub = transform.parent.GetComponentInChildren<CharAimInfoHub>();
-	}
-	
-	// Update is called once per frame
-	public void Update () 
-    {
-        //Debug.Log("animation");
-        if (in_animation && (Time.time - animation_start) > animation_length)
+        for (int i = 0; i < ray_cast_angles.Length; ++i)
         {
-            in_animation = false;
-            animation.enabled = false;
+            float a = ray_cast_angles[i] + aim_info_hub.GetAimRotation();
+            Vector2 ray = new Vector2(Mathf.Cos(a), Mathf.Sin(a));
+            Debug.DrawLine((Vector2)transform.position + ray * swing_start_radius, (Vector2)transform.position + ray * swing_radius);
         }
-	}
+    }
+
 }
--- KORT/Assets/Scripts/Action Scripts/Weapons/TestMelee.cs
+++ KORT/Assets/Scripts/Action Scripts/Weapons/TestMelee.cs
@@ -4,16 +4,5 @@ using System.Collections;
 public class TestMelee : MeleeBase 
 {
 
-	// Use this for initialization
-	void Start () 
-    {
-        weapon_name = "Test Weapon Melee";
-	}
 	
-	// Update is called once per frame
-	void Update () 
-    {
-        /// Delete this if you are doing your own animation management here
-        base.Update();
-	}
 }
