4256d117a0483eae8f33d833c31d3d9cd308a327
Robert Wild <wildr2@rpi.edu>
2014-11-21 15:12:48 -0500

    World text message

---
+++ KORT/Assets/Scripts/WorldTextMessage.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class WorldTextMessage : MonoBehaviour 
+{
+    public float fade_time = 1;
+    public TextMesh text_mesh;
+    private float timer;
+
+    public void Start()
+    {
+        timer = fade_time;
+    }
+
+    public void Update()
+    {
+        timer -= Time.deltaTime;
+
+        text_mesh.color = new Color(text_mesh.color.r, text_mesh.color.g,
+            text_mesh.color.b, timer / fade_time);
+
+        if (timer <= 0)
+            GameObject.Destroy(this);
+    }
+}
